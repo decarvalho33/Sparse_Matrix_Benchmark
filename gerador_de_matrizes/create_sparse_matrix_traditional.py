c2addc091613bde395db6a3b293e4b6f8aa1cf0b
decarvalho33 <alvarodecarvalho33@gmail.com>
2025-11-06 17:57:35 -0300

    add matrix generator

---
+++ gerador_de_matrizes/create_sparse_matrix_traditional.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def create_sparse_matrix_traditional(i, sparsity):
+    rows = cols = pow(10, i)
+
+    sparce_matrix = np.zeros((rows, cols), dtype=np.float32)
+    total = rows * cols
+    non_zero = int(total * sparsity)
+    indices = np.random.choice(total, non_zero, replace=False)
+    for index in indices:
+        row = index // cols
+        col = index % cols
+        sparce_matrix[row, col] = np.random.rand()
+    return sparce_matrix
+
+print(create_sparse_matrix_traditional(6, 6, 0.2))
