6c1471a609c6b585fe50d2aa816416c6e6250c52
decarvalho33 <alvarodecarvalho33@gmail.com>
2025-11-20 20:31:12 -0300

    pickles

--- example_read_matrix.py
+++ example_read_matrix.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import timeit
 import csv
-import pickle  # <-- ADICIONADO
+import pickle
 
 OP = "Inserção"
 
 dados = []
 
-for i in range(2, 3):
+for i in range(2, 7):
     if i >= 4:
         sparcities = [1/10**(i+2), 1/10**(i+1), 1/10**i]
     else:
@@ -22,12 +22,36 @@ for i in range(2, 3):
         sparcity_str = str(sparcity).replace('.', '_')
         matrix_file = f"matrices/matrices_i{i}_s{sparcity_str}.pkl"
 
-        setup_code = f"""
+        # ---------------------------------------------------------
+        # SE i > 3 → NÃO carregar matriz tradicional
+        # ---------------------------------------------------------
+        if i > 3:
+            setup_code = f"""
 import pickle
 from algorithm_1_hash import Sparse_matrix_hash
 from algorithm_2_AVL import Sparse_matrix_AVL
 
-# carrega as matrizes já geradas
+with open(r"{matrix_file}", "rb") as f:
+    data = pickle.load(f)
+
+# Tradicional não carregada
+trad_A = None
+trad_B = None
+
+A_hash = data["A_hash"]
+B_hash = data["B_hash"]
+
+A_avl = data["A_avl"]
+B_avl = data["B_avl"]
+            """
+
+            benchmark_trad = "pass"   # NÃO medir tradição
+        else:
+            setup_code = f"""
+import pickle
+from algorithm_1_hash import Sparse_matrix_hash
+from algorithm_2_AVL import Sparse_matrix_AVL
+
 with open(r"{matrix_file}", "rb") as f:
     data = pickle.load(f)
 
@@ -39,35 +63,42 @@ B_hash = data["B_hash"]
 
 A_avl = data["A_avl"]
 B_avl = data["B_avl"]
-        """
+            """
 
-        benchmark_hash = """
-A_hash.insert(50, 50, 1)
-        """
-
-        benchmark_avl = """
-A_avl[50, 50] = 1
-        """
-
-        benchmark_trad = """
+            benchmark_trad = """
 trad_A[50][50] = 1
-        """
+            """
+
+        benchmark_hash = "A_hash.insert(50, 50, 1)"
+        benchmark_avl  = "A_avl[50, 50] = 1"
 
         execution_time_hash = timeit.repeat(stmt=benchmark_hash, setup=setup_code, number=10000, repeat=1)
         hash_times.append(execution_time_hash[0])
-        
+
         execution_time_avl = timeit.repeat(stmt=benchmark_avl, setup=setup_code, number=10000, repeat=1)
         avl_times.append(execution_time_avl[0])
-        
-        execution_time_trad = timeit.repeat(stmt=benchmark_trad, setup=setup_code, number=10000, repeat=1)
-        trad_times.append(execution_time_trad[0])
 
-        dados.append([i, sparcity, execution_time_hash[0], execution_time_avl[0], execution_time_trad[0]])
+        # ----------------------------------------------
+        # SE i > 3, pula tradicional e usa valor nulo
+        # ----------------------------------------------
+        if i > 3:
+            trad_time = None
+        else:
+            execution_time_trad = timeit.repeat(stmt=benchmark_trad, setup=setup_code, number=10000, repeat=1)
+            trad_time = execution_time_trad[0]
+
+        trad_times.append(trad_time)
+
+        dados.append([i, sparcity, execution_time_hash[0], execution_time_avl[0], trad_time])
 
     plt.figure(figsize=(8,5))
     plt.plot(sparcities, hash_times, marker='o', label='Hash')
     plt.plot(sparcities, avl_times, marker='s', label='AVL')
-    plt.plot(sparcities, trad_times, marker='.', label='Trad')
+
+    # só plota o tradicional se existir valores
+    if any(x is not None for x in trad_times):
+        plt.plot(sparcities, trad_times, marker='.', label='Trad')
+
     plt.xlabel('Sparsity')
     plt.ylabel('Execution Time (s)')
     plt.title(f"Execução de Inserção (i={i})")
