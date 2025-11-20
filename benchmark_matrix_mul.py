dfd68fd33e90138a65c88f3761aee32f8cf36bbc
decarvalho33 <alvarodecarvalho33@gmail.com>
2025-11-20 18:52:58 -0300

    hash bench ok

--- benchmark_matrix_mul.py
+++ benchmark_matrix_mul.py
@@ -19,7 +19,7 @@ for i in range(2, 3):
     for sparcity in sparcities:
         setup_code = f"""
 from create_sparse_matrix_traditional import create_sparse_matrix_traditional
-#from algorithm_1_hash.py import Sparse_matrix_hash
+from algorithm_1_hash import Sparse_matrix_hash
 from algorithm_2_AVL import Sparse_matrix_AVL
 
 trad_A = create_sparse_matrix_traditional({i}, 0.01)
@@ -28,12 +28,12 @@ trad_B = create_sparse_matrix_traditional({i}, 0.01)
 A_avl = Sparse_matrix_AVL(trad_A)
 B_avl = Sparse_matrix_AVL(trad_B)
 
-#A_hash = Sparse_matrix_hash(trad_A)
-#B_hash = Sparse_matrix_hash(trad_A)
+A_hash = Sparse_matrix_hash(trad_A)
+B_hash = Sparse_matrix_hash(trad_B)
         """
 
         benchmark_hash = """
-B_avl[50, 50] = 1
+A_hash.times_matrix(B_hash)
         """
 
         benchmark_avl = """
