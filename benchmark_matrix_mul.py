ab4a1725870b3c29c90f9af2b804e1e8f50587a4
Eduardo Tejada <duduptejada@gmail.com>
2025-11-20 19:22:54 -0300

    mmul

--- benchmark_matrix_mul.py
+++ benchmark_matrix_mul.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 import timeit
 import csv
 
-OP = "Inserção"
+OP = "Multiplicação"
 
 dados = []
 
-for i in range(2, 6):
+for i in range(2, 7):
     if i >= 4:
         sparcities = [1/10**(i+2), 1/10**(i+1), 1/10**i]
     else:
